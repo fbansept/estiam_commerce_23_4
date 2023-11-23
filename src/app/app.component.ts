619b09e519f1fd40b4d6500fda4c6c1c7678fc60
fbansept <bansept.franck@gmail.com>
2023-11-23 16:38:49 +0100

    part 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ProductPricePipe } from './pipes/product-price.pipe';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
